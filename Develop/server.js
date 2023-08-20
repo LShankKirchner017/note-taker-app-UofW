4b609ac8606f021648c8cb4a0d97fa9369d5094d
Lindsay <lshank017@gmail.com>
2023-08-20 14:29:25 -0500

    created routes folder and files, linked routes in server

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const routes = require(/routes/routes)
+const apiRoutes = requires(/routes/apiRoutes)
+const PORT = 3001;
+const app = express();
+
+app.use(express)
+app.use(routes)
+app.use(apiRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
