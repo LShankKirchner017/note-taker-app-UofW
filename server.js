a86bb9e99590d5f66a4a8573389d2d3778efbeec
Lindsay <lshank017@gmail.com>
2023-08-21 16:29:27 -0500

    bug fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const pageRoutes = require("./routes/pageRoutes");
 const apiRoutes = require("./routes/apiRoutes");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Middleware
