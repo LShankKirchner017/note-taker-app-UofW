d7ab8926a13734038cc9dacc5a916ffb2f67c3a8
Lindsay <lshank017@gmail.com>
2023-08-21 15:43:42 -0500

    finished api routes

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -17,9 +17,21 @@ router.get("/api/notes", async (req, res) =>{
 })
 
 // POST Route
-router.post("/api/notes")
-
-// Delete Route
+router.post("/api/notes", async (req,res) => {
+  try{
+    const notes = JSON.parse(await fs.readFile(dbPath, "utf-8"))
+    notes.push({
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4()
+    })
+    await fs.writeFile(dbPath, JSON.stringify(notes))
+    res.json(notes);
+  } catch(err){
+    res.status(500).send(err)
+    console.log("An error occurred")
+  }
+})
 
 
 
