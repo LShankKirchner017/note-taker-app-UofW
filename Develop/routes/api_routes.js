61dd1459cdc5630855635d1f841788641bd7fb16
Lindsay <lshank017@gmail.com>
2023-08-21 15:31:33 -0500

    installed uuid

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const path = require('path')
 con
+const fs = require('fs/promises')
+const dbPath = path.join(__dirname, "..", "db", "db.json")
+const { v4: uuidv4 } = require('uuid')
 
 
 
@@ -10,6 +13,4 @@ con
 
 
 
-
-
-module.exports = api_routes
+module.exports = router
--- README.md
+++ README.md
@@ -27,3 +27,5 @@ WHEN I click on the Write icon in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 ```
 
+## Resources Used 
+https://dev.to/gulshanaggarwal/npm-packages-to-generate-unique-ids-for-your-next-project-1p3b
