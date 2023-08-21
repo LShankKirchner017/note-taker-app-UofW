3e389057385cfbec7eb38db2fbe5fd3d118d97fa
Lindsay <lshank017@gmail.com>
2023-08-21 17:55:35 -0500

    added new note const

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,39 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs/promises")
-const dbPath = path.join(__dirname, "..", "db", "db.json")
+dbFilePath = path.join(__dirname, "..", "db", "db.json")
 const nanoid = require('nanoid')
 
 
 // GET route
-router.get("/api/notes", async (req, res) => {
-  try {
-    const parsedDbJson = JSON.parse(await fs.readFile(dbPath))
-    res.json(parsedDbJson)
+router.get('/api/notes', async (req,res) => {
+  try{
+    const notes = await JSON.parse(await fs.readFile(dbFilePath))
+    res.json(notes)
   } catch (err) {
     res.status(500).send(err)
-    console.error(err)
+    console.log("An error occurred")
   }
-});
+})
 
 // POST route
-router.post("/api/notes", async (req, res) => {
-  try {
-    const notes = JSON.parse(await fs.readFile(dbPath, "utf-8"))
-    notes.push({
+router.post("/api/notes", async (req,res) => {
+  try{
+    const content = await fs.readFile(dbFilePath, 'utf-8')
+    const notes = JSON.parse(content)
+    
+    const newNote = {
       title: req.body.title,
       text: req.body.text,
       id: nanoid()
-    })
-    await fs.writeFile(dbPath, JSON.stringify(notes))
-    res.json(notes)
-  } catch(err) {
+    }
+    notes.push(newNote)
+    await fs.writeFile(dbFilePath, JSON.stringify(notes))
+
+    res.status(201).json(notes)
+  } catch (err) {
     res.status(500).send(err)
-    console.error(err)
+    console.log("An error occurred.")
   }
 })
 
