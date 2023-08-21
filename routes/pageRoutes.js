4aff9d286572bcc051691dcbfd0f519f5be766df
Lindsay <lshank017@gmail.com>
2023-08-21 18:37:34 -0500

    debugging reset

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,18 +1,1 @@
-const router = require("express").Router();
-const path = require("path");
 
-// route for the index page
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
-})
-
-// route for the notes page
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "notes.html"))
-})
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
-})
-
-module.exports = router;
